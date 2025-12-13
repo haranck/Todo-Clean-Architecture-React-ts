547b5942900c14b4cef6fa3b00e1529d3dfcc738
Haranck <harankrishna03@gmail.com>
2025-12-13 10:25:49 +0530

    todoRoutes and main file changed to class

--- todo/backend/src/presentation/routes/todoRoutes.ts
+++ todo/backend/src/presentation/routes/todoRoutes.ts
@@ -1,18 +1,31 @@
 import { Router } from "express";
+import { TodoController } from "../controllers/todoController";
+import { TodoRepository } from "../../infrastructure/database/repository/todoRepository";
+import { TodoService } from "../../infrastructure/adapters/todoService";
 
-import{TodoController} from '../controllers/todoController'
-import {TodoRepository} from '../../infrastructure/database/repository/todoRepository'
-import {TodoService} from '../../infrastructure/adapters/todoService'
+export class TodoRoute {
+  private readonly router: Router;
+  private readonly controller: TodoController;
 
+  constructor() {
+    this.router = Router();
 
-const router = Router();
+    const repo = new TodoRepository();
+    const service = new TodoService(repo);
+    this.controller = new TodoController(service);
 
-const repo = new TodoRepository()
-const service = new TodoService(repo)
-const controller = new TodoController(service)
+    this.setRoutes();
+  }
 
-router.get('/',controller.list)
-router.post('/',controller.create)
-router.delete('/:id',controller.delete)
+  private setRoutes(): void {
+    this.router.get("/", this.controller.list.bind(this.controller));
 
-export default router
+    this.router.post("/", this.controller.create.bind(this.controller));
+
+    this.router.delete("/:id", this.controller.delete.bind(this.controller));
+  }
+
+  public getRoutes(): Router {
+    return this.router;
+  }
+}
